d2b58da51fb6673449e52486b71057fbe18b4b58
Arely Martinez <64700714+arelymartinez16@users.noreply.github.com>
2024-12-05 19:45:34 -0500

    Update edit profile url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/edit-profile" element={<EditProfile />}></Route>
+        <Route path="/edit-profile:id" element={<EditProfile />}></Route>
       </Routes>
     </>
   )
