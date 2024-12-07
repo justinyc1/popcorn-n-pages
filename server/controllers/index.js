bbcb998749e81985a07f84671c9b6292e3d3b423
justinyc1 <justinyijiachen@gmail.com>
2024-12-06 22:24:45 -0500

    setup to test creators table

---
+++ server/controllers/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+// TODO: load your controllers here
+import creatorsController from "./creators.js";
+
+// TODO: mount your controllers here to a URL path
+const router = express.Router();
+router.use("creators", creatorsController);
+
+export default router;
