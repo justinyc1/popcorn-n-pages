5dfeae9907aa3c5b37b373607eeda49020b756be
justinyc1 <justinyijiachen@gmail.com>
2025-05-03 18:14:49 -0400

    navbar display loading by during auth

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdClose } from "react-icons/io";
 import { useAuth } from "../auth/UseAuth";
 
 const Navbar = () => {
-    //eslint-disable-next-line no-unused-vars
     const { isAuthenticated, logout } = useAuth();
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
@@ -54,7 +53,7 @@ const Navbar = () => {
                         Books
                     </Link>
                     {isAuthenticated === null &&
-                        <span>Loading...</span>
+                        <span className="inline-flex font-semibold transition-opacity cursor-pointer border-t-4 border-b-2 border-transparent">Loading...</span>
                     }
                     {isAuthenticated &&
                         <>
@@ -68,7 +67,7 @@ const Navbar = () => {
                             </Link>
                         </>
                     }
-                    {!isAuthenticated && 
+                    {isAuthenticated === false && 
                         <>
                             <Link to="/login">
                                 <button className="border border-white px-4 py-1 rounded font-semibold hover:bg-lightorange-lightest hover:text-lightblue-darker transition ease-in-out duration-200">
@@ -89,30 +88,37 @@ const Navbar = () => {
             {isMobileMenuOpen && (
                 <div className="lg:hidden absolute inset-x-0 top-[70px] bg-teal-700 text-white shadow-md">
                     <nav className="flex flex-col gap-4 p-4">
-                        <Link to="/" className="hover:text-teal-300">
+                        <Link to="/"  className="inline-flex font-semibold hover:text-lightorange transition-opacity cursor-pointer border-t-4 border-b-2 border-transparent hover:border-b-lightorange">
                             Home
                         </Link>
-                        <Link to="/movies" className="hover:text-teal-300">
+                        <Link to="/movies"  className="inline-flex font-semibold hover:text-lightorange transition-opacity cursor-pointer border-t-4 border-b-2 border-transparent hover:border-b-lightorange">
                             Movies
                         </Link>
-                        <Link to="/tv-shows" className="hover:text-teal-300">
+                        <Link to="/tv-shows"  className="inline-flex font-semibold hover:text-lightorange transition-opacity cursor-pointer border-t-4 border-b-2 border-transparent hover:border-b-lightorange">
                             TV Shows
                         </Link>
-                        <Link to="/books" className="hover:text-teal-300">
+                        <Link to="/books"  className="inline-flex font-semibold hover:text-lightorange transition-opacity cursor-pointer border-t-4 border-b-2 border-transparent hover:border-b-lightorange">
                             Books
                         </Link>
-                        <Link to="/community" className="hover:text-teal-300">
+                        <Link to="/community"  className="inline-flex font-semibold hover:text-lightorange transition-opacity cursor-pointer border-t-4 border-b-2 border-transparent hover:border-b-lightorange">
                             Community
                         </Link>
                         {isAuthenticated === null &&
-                            <span>Loading...</span>
+                            <span className="inline-flex font-semibold transition-opacity cursor-pointer border-t-4 border-b-2 border-transparent">Loading...</span>
                         }
                         {isAuthenticated &&
-                            <Link to="/profile" className="hover:text-teal-300">
-                                My Profile
-                            </Link>
+                            <>
+                                <Link to="/profile" className="border border-white px-4 py-1 rounded hover:bg-white hover:text-teal-700 transition ease-in-out duration-200">
+                                    My Profile
+                                </Link>
+                                <Link to="/">
+                                    <button onClick={handleLogout} className="border border-white px-4 py-1 rounded font-semibold hover:bg-lightorange-lightest hover:text-lightblue-darker transition ease-in-out duration-200">
+                                        Logout
+                                    </button>
+                                </Link>
+                            </>
                         }
-                        {!isAuthenticated &&
+                        {isAuthenticated === false &&
                             <>
                                 <Link to="/login">
                                     <button className="border border-white px-4 py-1 rounded hover:bg-white hover:text-teal-700 transition ease-in-out duration-200">
