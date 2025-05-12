fbe7b14cb35e3b1dad041d4db823f3d49117d8fe
justinyc1 <justinyijiachen@gmail.com>
2025-05-12 00:56:14 -0400

    fixed tastedive bug

--- server/services/mediaService.js
+++ server/services/mediaService.js
@@ -9,20 +9,19 @@ const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const geminiAi = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 export const fetchTasteDive = async (searchQuery, mediaType) => {
-    const tasteDiveResponse = await fetch(`
-            https://tastedive.com/api/similar
-            ?q=${searchQuery}
-            &type=${mediaType}
-            &info=1
-            &k=${TASTEDIVE_API_KEY}
-    `);
-    
+    const tasteDiveResponse = await fetch(`https://tastedive.com/api/similar?q=${searchQuery}&type=${mediaType}&info=1&k=${TASTEDIVE_API_KEY}`);
+    console.log("DEBUG 2");
+    console.log(tasteDiveResponse);
     const jsonData = await tasteDiveResponse.json();
+    console.log("DEBUG 3");
+    console.log(jsonData);
+    console.log("DEBUG 4");
     return jsonData;
 }
 
 export const fetchGeminiAi = async (count, mediaName, mediaType) => {
     const model = geminiAi.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
+    console.log(mediaType);
     const prompt = `return me a json object, delimited by backticks, of ${count} 
             existing ${mediaType}s similar to ${mediaName}, with the 
             ${mediaType === "book" ? "authors" : "directors, main actors"}, 
@@ -39,7 +38,9 @@ export const fetchGeminiAi = async (count, mediaName, mediaType) => {
         return response;
     }
     const jsonString = trimResponseDelimiters(result.response.text());
+    console.log(jsonString);
     const jsonData = JSON.parse(jsonString);
+    console.log(jsonData);
     return jsonData;
 }
 
@@ -68,31 +69,19 @@ export const fetchImages = async (jsonResults) => {
 }
 
 export const fetchMovieData = async (mediaName) => {
-    const tmdbResponse = await fetch(`
-            https://api.themoviedb.org/3/search/movie
-            ?api_key=${TMDB_API_KEY}
-            &query=${mediaName}
-    `);
+    const tmdbResponse = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${mediaName}`);
     const jsonData = await tmdbResponse.json();
     return jsonData;
 }
 
 export const fetchTVShowData = async (mediaName) => {
-    const tmdbResponse = await fetch(`
-            https://api.themoviedb.org/3/search/tv
-            ?api_key=${TMDB_API_KEY}
-            &query=${mediaName}
-    `);
+    const tmdbResponse = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${TMDB_API_KEY}&query=${mediaName}`);
     const jsonData = await tmdbResponse.json();
     return jsonData;
 }
 
 export const fetchBookData = async (mediaName) => {
-    const bookResponse = await fetch(`
-            https://www.googleapis.com/books/v1/volumes
-            ?q=intitle:${mediaName}
-            &key=${GOOGLE_BOOKS_API_KEY}
-    `);
+    const bookResponse = await fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${mediaName}&key=${GOOGLE_BOOKS_API_KEY}`);
     const jsonData = await bookResponse.json();
     return jsonData;
 }
